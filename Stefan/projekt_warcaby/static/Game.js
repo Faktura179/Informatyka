ca54f114d22528856d31adedf3d20686c06e81ef
Faktura179 <rfatula12@gmail.com>
2019-03-30 21:01:29 +0100

    projekt warcaby ruszanie sie po wolnych polach bez obostrzen

--- Stefan/projekt_warcaby/static/Game.js
+++ Stefan/projekt_warcaby/static/Game.js
@@ -1,37 +1,15 @@
-class WhiteTile{
-    constructor(){
-        var geometry = new THREE.BoxGeometry(100, 40, 100);
-        var materials = [];
-        for(var i=0;i<6;i++)materials.push(new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('img/white_tile2.jpg') }));
-        var cube = new THREE.Mesh(geometry, materials);
-        return cube
+class Tile extends THREE.Mesh{
+    constructor(geometry,mesh){
+        if(arguments.length==1){
+            var geo = new THREE.BoxGeometry(100, 40, 100);
+            var mat = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load(geometry) });
+            super(geo,mat)
+        }else{
+            super(geometry,mesh)
+        }
     }
 }
-class BlackTile{
-    constructor(){
-        var geometry = new THREE.BoxGeometry(100, 40, 100);
-        var materials = [];
-        for(var i=0;i<6;i++)materials.push(new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('img/black_tile2.jpg') }));
-        var cube = new THREE.Mesh(geometry, materials);
-        return cube
-    }
-}
-class WhitePiece{
-    constructor(){
-        var geometry = new THREE.CylinderGeometry(40,40,20,40)
-        var mesh = new THREE.MeshBasicMaterial({side: THREE.DoubleSide, map: new THREE.TextureLoader().load("img/white_pawn.jpg")})
-        var cylinder = new THREE.Mesh(geometry, mesh)
-        return cylinder
-    }
-}
-class BlackPiece{
-    constructor(){
-        var geometry = new THREE.CylinderGeometry(40,40,20,40)
-        var mesh = new THREE.MeshBasicMaterial({side: THREE.DoubleSide, map: new THREE.TextureLoader().load("img/red_pawn.jpg")})
-        var cylinder = new THREE.Mesh(geometry, mesh)
-        return cylinder
-    }
-}
+
 class Game{
     constructor(){
         var scene = new THREE.Scene();
@@ -53,13 +31,18 @@ class Game{
         });
         var axes = new THREE.AxesHelper(1000)
         scene.add(axes)
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
 
         this.scene=scene
         this.camera=camera
         this.renderer=renderer
+        this.selectedPiece=null
 
         this.render()
         window.onresize=this.resize.bind(this)
+        renderer.domElement.addEventListener( 'mousemove', this.onMouseMove.bind(this), false );
+        renderer.domElement.onclick=this.onClick.bind(this)
 
 
         this.szachownica=[
@@ -85,16 +68,20 @@ class Game{
 
 
         //Generuj plansze
-        var blackTile = new BlackTile()
-        var whiteTile = new WhiteTile()
+        var blackTile = new Tile("img/black_tile2.jpg")
+        var whiteTile = new Tile('img/white_tile2.jpg')
+        this.blackTiles=[]
         for(var i =0;i<this.szachownica.length;i++){
             for(var j=0;j<this.szachownica[i].length;j++){
                 var cube
-                if(this.szachownica[i][j]==0){
-                    cube =blackTile.clone()
-                }else{
+                if(this.szachownica[i][j]==1){
                     cube =whiteTile.clone()
+                }else{
+                    cube =blackTile.clone()
+                    this.blackTiles.push(cube)
                 }
+                cube.x=j
+                cube.y=i
                 cube.position.x=-350+j*100
                 cube.position.z=-350+i*100
                 scene.add(cube)
@@ -118,22 +105,65 @@ class Game{
     }
 
     setPieces(){
-        var whitePiece = new WhitePiece()
-        var blackPiece = new BlackPiece()
+        var whitePiece = new Piece("/img/white_pawn.jpg")
+        var blackPiece = new Piece("/img/red_pawn.jpg")
+        this.whitePieces=[]
+        this.blackPieces=[]
         for(var i =0;i<this.pionki.length;i++){
             for(var j=0;j<this.pionki[i].length;j++){
                 var piece
                 if(this.pionki[i][j]==1){
                     piece = whitePiece.clone()
+                    this.whitePieces.push(piece)
                 }else if(this.pionki[i][j]==2){
                     piece = blackPiece.clone()
+                    this.blackPieces.push(piece)
                 }else{
                     continue
                 }
+                piece.x=j
+                piece.y=i
                 piece.position.x=-350+j*100
                 piece.position.z=-350+i*100
                 piece.position.y=20
                 this.scene.add(piece)
+            }
+        }
+    }
+    onMouseMove( event ) {
+
+        // calculate mouse position in normalized device coordinates
+        // (-1 to +1) for both components
+    
+        this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    }
+
+    onClick(event){
+        this.raycaster.setFromCamera( this.mouse, this.camera );
+        var arr = net.player==1 ? this.whitePieces:this.blackPieces
+        var intersects =this.raycaster.intersectObjects( arr, true )
+        if(intersects.length>0){
+            arr.forEach(el=>{
+                el.color=0xffffff
+            })
+            intersects[ 0 ].object.color= 0x00ffff
+            this.selectedPiece=intersects[0].object
+        }
+        var intersects =this.raycaster.intersectObjects( this.blackTiles, true )
+        if(intersects.length>0){
+            if(this.selectedPiece!=null){
+                var pos = intersects[0].object.position
+                var x = intersects[0].object.x
+                var y = intersects[0].object.y
+                if(this.pionki[y][x]==0){
+                    this.selectedPiece.position.set(pos.x,20,pos.z)
+                    this.pionki[y][x]=net.player
+                    this.pionki[this.selectedPiece.y][this.selectedPiece.x]=0
+                    this.selectedPiece.x=x
+                    this.selectedPiece.y=y
+                }
             }
         }
     }
