314077f54c08da2fdd291fc9ad0bc061750631cd
Faktura179 <rfatula12@gmail.com>
2019-06-02 22:34:10 +0200

    Monetki 1 etap

--- Nelson/Monetki/server.php
+++ Nelson/Monetki/server.php
@@ -4,9 +4,22 @@
         $query = $conn->query("SELECT * FROM Monetki");
         $row = $query->fetch_all();
         echo(json_encode($row));
+    }else if(isset($_POST['f']) && $_POST['f']=='update'){
+        $sql =  "UPDATE Monetki SET Kraj='".$_POST["kraj"]."', Nominal='".$_POST["nominal"]."', Kategoria='".$_POST["kategoria"]."', Stop='".$_POST["stop"]."', Rok='".$_POST["rok"]."' WHERE ID=".$_POST["id"];
+        $resp = $conn->query($sql);
+        print_r($resp);
+    }else if(isset($_POST['f']) && $_POST['f']=='delete'){
+        $sql = "DELETE FROM Monetki WHERE ID=".$_POST["id"];
+        $resp = $conn->query($sql);
+        print_r($resp);
+    }else if(isset($_POST['f']) && $_POST['f']=='add'){
+        $sql = "INSERT INTO Monetki (Kraj,Nominal,Kategoria,Stop,Rok) VALUES('".$_POST["kraj"]."','".$_POST["nominal"]."','".$_POST["kategoria"]."','".$_POST["stop"]."','".$_POST["rok"]."')";
+        $resp = $conn->query($sql);
+        print_r($resp);
     }
 
-    
 
+    
+    $conn->close();
 
 ?>
